42460680b4f2ea09c1b1ac8ba9da5347cbe324dd
unknown <roxliro@yande.com>
2024-05-23 17:59:41 +0800

    first commit

---
+++ bitrix/modules/iblock/install/components/bitrix/iblock.field.section/lang/ru/class.php
@@ -0,0 +1,2 @@
+<?php
+$MESS['IBLOCK_FIELD_SECTION_CATALOG_ACCESS_DENIED'] = "Ќедостаточно прав дл€ доступа к каталогу";
---
+++ bitrix/modules/iblock/lang/ru/lib/grid/panel/groupaction.php
@@ -0,0 +1,18 @@
+<?php
+$MESS['IBLOCK_GRID_PANEL_ACTION_CLEAR_COUNTER'] = "сбросить счетчик просмотров";
+$MESS['IBLOCK_GRID_PANEL_ACTION_CLEAR_COUNTER_CONFIRM'] = "¬ы уверены, что хотите обнулить счетчик и дату начала просмотров у выбранных элементов?";
+$MESS['IBLOCK_GRID_PANEL_ACTION_CODE_TRANSLITERATION'] = "создать символьный код";
+$MESS['IBLOCK_GRID_PANEL_ACTION_CODE_TRANSLITERATION_CONFIRM'] = "¬ы уверены, что хотите сгенерировать символьные коды дл€ выбранных позиций?";
+$MESS['IBLOCK_GRID_PANEL_ACTION_MESS_SECTION_TOP_LEVEL'] = "¬ерхний уровень";
+$MESS['IBLOCK_GRID_PANEL_ACTION_ADD_SECTION'] = "добавить прив€зку к разделу";
+$MESS['IBLOCK_GRID_PANEL_ACTION_ADJUST_SECTION'] = "перенести в раздел";
+$MESS['IBLOCK_GRID_PANEL_ACTION_ACTIVATE_MSGVER_1'] = "активировать";
+$MESS['IBLOCK_GRID_PANEL_ACTION_DEACTIVATE_MSGVER_1'] = "деактивировать";
+$MESS['IBLOCK_GRID_PANEL_ACTION_DELETE'] = "удалить";
+$MESS['IBLOCK_GRID_PANEL_ACTION_EDIT'] = "редактировать";
+$MESS['IBLOCK_GRID_PANEL_ACTION_ELEMENT_UNLOCK'] = "разблокировать";
+$MESS['IBLOCK_GRID_PANEL_ACTION_ELEMENT_LOCK'] = "заблокировать";
+$MESS['IBLOCK_GRID_PANEL_ACTION_ELEMENT_WORKFLOW_STATUS'] = "изменить статус";
+
+$MESS['IBLOCK_GRID_PANEL_ACTION_ACTIVATE_MSGVER_2'] = "активировать";
+$MESS['IBLOCK_GRID_PANEL_ACTION_DEACTIVATE_MSGVER_2'] = "деактивировать";
